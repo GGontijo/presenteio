dbc333e2f8eabdce59cecc0630f7e3cfb7f84a01
Dercilio <38219914+GGontijo@users.noreply.github.com>
2025-04-06 17:29:24 -0400

    fix: corrigir configuração de ambiente no logging da aplicação

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,15 +2,14 @@ import logging
 import os
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
 from app.database import load_up_tables
 from app.routes.domains_router import domain_router
 from app.routes.items_router import item_router
 from app.routes.pages_router import page_item_router, page_router
 from app.routes.uploads_router import uploads_router
 from app.routes.users_router import users_router
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # TODO: Obter User ID do token
 # TODO: Melhorar logging em geral, usar repr etc.
@@ -21,7 +20,7 @@ from app.routes.users_router import users_router
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # starts up development environment
-    logging.info(f"Starting with {os.getenv('ENV')} environment...")
+    logging.info(f"Starting with {os.getenv('ENV'), 'development'} environment...")
     match os.getenv("ENV"):
         case "development":
             load_up_tables()
